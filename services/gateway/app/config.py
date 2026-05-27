ae4a465fa74bdcb13c6bf4ab809fba235f276401
Tristan-Tenders <tristangandi@gmail.com>
2026-05-27 22:21:22 +0200

    module-03: gateway + activity-service inter-service calls + reflection

---
+++ services/gateway/app/config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    user_service_url: str = "http://localhost:8001"
+    game_service_url: str = "http://localhost:8002"
+    activity_service_url: str = "http://localhost:8003"
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
